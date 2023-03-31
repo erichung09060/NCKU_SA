4a210f065e27157c27bf931730691f6d8408542a
洪睿廷 <erichung@MacBook-Air.local>
2023-03-31 10:38:44 +0800

    score:100

--- HW2/sahw2.sh
+++ HW2/sahw2.sh
@@ -1,6 +1,3 @@
-# #!/usr/local/bin/bash.
-#!/bin/bash
-
 usage() {
     echo -e "\nUsage: sahw2.sh {--sha256 hashes ... | --md5 hashes ...} -i files ...\n\n--sha256: SHA256 hashes to validate input files.\n--md5: MD5 hashes to validate input files.\n-i: Input files."
 }
@@ -9,43 +6,49 @@ sha256=false
 md5=false
 hashVal=()
 fileName=()
-curType=""
 
-until [ $# -eq 0 ]; do
+while [[ $# != 0 ]]; do
     if [ $1 == "-h" ]; then
         usage
         exit 0
     elif [ $1 == "--sha256" ]; then
         sha256=true
-        curType="hash"
+        shift
+        while [[ $# != 0 && ! $1 =~ ^- ]]; do
+            hashVal+=($1)
+            shift
+        done
     elif [ $1 == "--md5" ]; then
         md5=true
-        curType="hash"
+        shift
+        while [[ $# != 0 && ! $1 =~ ^- ]]; do
+            hashVal+=($1)
+            shift
+        done
     elif [ $1 == "-i" ]; then
-        curType="file"
-    elif [ ${1:0:1} == "-" ]; then
+        shift
+        while [[ $# != 0 && ! $1 =~ ^- ]]; do
+            fileName+=($1)
+            shift
+        done
+    else
         echo -e "Error: Invalid arguments." 1>&2
         usage
         exit 1
-    elif $sha256 && $md5; then
-        echo "Error: Only one type of hash function is allowed." 1>&2
-        exit 1
-    elif [ $curType == "file" ]; then
-        fileName+=($1)
-    elif [ $curType == "hash" ]; then
-        hashVal+=($1)
     fi
-    shift
 done
 
-if [ ${#fileName[@]} != ${#hashVal[@]} ]; then
+if $sha256 && $md5; then
+    echo "Error: Only one type of hash function is allowed." 1>&2
+    exit 1
+elif [ ${#fileName[@]} != ${#hashVal[@]} ]; then
     echo "Error: Invalid values." 1>&2
     exit 1
 fi
 
-N=${#fileName[@]}
+fileNumber=${#fileName[@]}
 
-for ((i = 0; i < $N; i++)); do
+for ((i = 0; i < $fileNumber; i++)); do
     if $md5; then
         hash=$(md5sum ${fileName[i]} | cut -d ' ' -f 1)
     else
@@ -58,24 +61,72 @@ for ((i = 0; i < $N; i++)); do
     fi
 done
 
-#  cat data.json | jq -r ".[] | .username"
-User=""
-for ((i = 0; i < $N; i++)); do
-    Content=$(file ${fileName[$i]} | cut -d ' ' -f 2)
+User=()
+Password=()
+Shell=()
+Groups=()
 
-    if [ $Content == "CSV" ]; then
-        User="$User $(awk -F ',' '{if (NR!=1) print $1}' ${fileName[$i]})"
+for ((t = 0; t < $fileNumber; t++)); do
+    FileType=$(file ${fileName[$t]} | cut -d ' ' -f 2)
 
-    elif [ $Content == "JSON" ]; then
-        User="$User $(cat ${fileName[$i]} | jq -r ".[] | .username")"
+    if [ $FileType == "CSV" ]; then
+        line=0
+        while IFS=',' read user password shell groups; do
+            if [[ $((++line)) == 1 ]]; then continue; fi
+            User+=($user)
+            Password+=($password)
+            Shell+=($shell)
+            Groups+=("$groups")
+        done < ${fileName[$t]}
+
+    elif [ $FileType == "JSON" ]; then
+        User+=($(jq -r ".[] | .username" ${fileName[$t]}))
+        Password+=($(jq -r ".[] | .password" ${fileName[$t]}))
+        Shell+=($(jq -r ".[] | .shell" ${fileName[$t]}))
+        group_tmp=$(jq -r ".[] | .groups" ${fileName[$t]})
+        groups=""
+        for i in $(echo $group_tmp | sed s/[\",]//g); do 
+            if [[ $i == "[]" ]]; then
+                Groups+=("")
+            elif [[ $i == "]" ]]; then
+                Groups+=("$groups")
+                groups=""
+            elif [[ $i == "[" ]]; then
+                continue
+            else
+                groups="$groups $i"
+            fi
+        done
     else
         echo "Error: Invalid file format." 1>&2
         exit 1
     fi
 done
 
-echo -n "This script will create the following user(s):" $User "Do you want to continue? [y/n]:"
+userNumber=${#User[@]}
+
+echo -n "This script will create the following user(s):" ${User[@]} "Do you want to continue? [y/n]:"
 read reply
+
 if [[ $reply == "n" || $reply == "" ]]; then
     exit 0
 fi
+
+for ((i = 0; i < $userNumber; i++)); do
+    if id ${User[$i]} &>/dev/null; then
+        echo "Warning: user ${User[$i]} already exists."
+        continue
+    fi
+    
+    if [[ ${Groups[$i]} == "" ]]; then
+        sudo pw useradd ${User[$i]} -s ${Shell[$i]}
+        echo ${Password[$i]} | sudo pw mod user ${User[$i]} -h 0
+    else
+        for j in ${Groups[$i]}; do
+            sudo pw addgroup $j >/dev/null 2>&1
+        done
+        Groups[$i]=$(echo ${Groups[$i]} | sed "s/ /,/g")
+        sudo pw useradd ${User[$i]} -s ${Shell[$i]} -G ${Groups[$i]}
+        echo ${Password[$i]} | sudo pw mod user ${User[$i]} -h 0
+    fi
+done
